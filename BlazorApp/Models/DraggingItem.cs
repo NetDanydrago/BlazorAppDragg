2b8e255678f0cacc57813194e74f5c46d007577f
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2022-09-01 13:48:13 -0500

    Firs

---
+++ BlazorApp/Models/DraggingItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class DraggingItem
+    {
+
+        public int Order { get; set; }
+        public string Value { get; set; }
+        public bool IsDragOver { get; set; }
+        public bool IsMove { get; set; }
+    }
+}
